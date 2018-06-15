1369e611462e6424dd74b5c381805b355528caa0
Marc Lynch <lynchmarc2@gmail.com>
2018-06-15 14:58:27 -0700

    build and dep updates

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,52 +1,145 @@
 /* globals module, require */
 'use strict';
 
-var typescript = require('broccoli-typescript-compiler').typescript;
-var Rollup = require('broccoli-rollup');
-var MergeTrees = require('broccoli-merge-trees');
-var replace = require('broccoli-string-replace');
+const MergeTrees = require('broccoli-merge-trees');
+const Funnel = require('broccoli-funnel');
+const Rollup = require('broccoli-rollup');
+const path = require('path');
+const typescript = require('broccoli-typescript-compiler').default;
+const buble = require('rollup-plugin-buble');
+const fs = require('fs');
+const SOURCE_MAPPING_DATA_URL = '//# sourceMap' + 'pingURL=data:application/json;base64,';
 
-module.exports = function() {
-  const es6Tree = typescript('src', {
+module.exports = function(app) {
+  const src = new MergeTrees([
+    new Funnel(__dirname + '/src', {
+      destDir: 'src'
+    }),
+    new Funnel(__dirname + '/tests', {
+      destDir: 'tests'
+    })
+  ]);
+
+  const qunit = new Funnel(path.dirname(require.resolve('qunit')), {
+    annotation: 'tests/qunit.{js,css}',
+    destDir: 'tests',
+    files: ['qunit.css', 'qunit.js']
+  });
+
+  const loader = new Funnel(path.dirname(require.resolve('loader.js')), {
+    annotation: 'tests/loader.js',
+    destDir: 'tests',
+    files: ['loader.js']
+  });
+
+  const tests = new Funnel(__dirname + '/tests', {
+    destDir: 'tests',
+    files: ['index.html']
+  });
+
+  const compiled = typescript(src, {
+    throwOnError: process.env.EMBER_ENV === 'production',
+  });
+
+  const compiledDeclarations = typescript('src', {
     tsconfig: {
       compilerOptions: {
-        "noImplicitAny": true,
         "declaration": true,
-        "isolatedModules": false,
-        "module": "es2015",
-        "target": "es2015",
-        "outDir": "es6",
-        "sourceMap": true,
-        "moduleResolution": "node"
-      },
-      filesGlob: [
-        "**/*.ts"
+      }
+    }
+  });
+
+  const es6Tree = typescript(compiled, {
+    annotation: 'es6tree',
+    rollup: {
+      input: 'src/index.js',
+      output: [{
+        file: 'es6/scalar.js',
+        format: 'es',
+        sourcemap: true,
+        exports: 'named'
+      }],
+      plugins: [
+        loadWithInlineMap()
       ]
     }
   });
 
-  const umdTree = replace(new Rollup(es6Tree, {
-    annotation: 'umdTree',
+
+  const amdNamed = new Rollup(compiled, {
     rollup: {
-      input: 'index.js',
-      external: ['scalar-js'],
+      input: 'src/index.js',
       output: [{
-        file: 'scalar-js.js',
-        format: 'umd',
+        file: 'named-amd/scalar.js',
         exports: 'named',
+        format: 'amd',
+        amd: { id: 'scalar' },
+        sourcemap: true
+      }, {
+        file: 'scalar.js',
+        format: 'cjs',
         sourcemap: true,
-        name: 'umd'
-      }]
+        exports: 'named'
+      }],
+      plugins: [
+        loadWithInlineMap(),
+        buble()
+      ]
     }
-  }), {
-      input: [ 'scalar-js.js' ],
-      pattern: {
-        match: /undefined.__extends/g,
-        replacement: 'false'
-      }
   });
 
-  return new MergeTrees([es6Tree, umdTree], {
+  const amdTests = new Rollup(compiled, {
+    annotation: 'named-amd/tests.js',
+    rollup: {
+      input: 'tests/index.js',
+      external: ['scalar'],
+      output: [{
+        file: 'named-amd/tests.js',
+        format: 'amd',
+        amd: { id: 'scalar-tests' },
+        sourcemap: true,
+        exports: 'named'
+      }],
+      plugins: [
+        loadWithInlineMap(),
+        buble()
+      ]
+    }
+  });
+
+  return new MergeTrees([
+    es6Tree,
+    compiledDeclarations,
+    amdNamed,
+    amdTests,
+    qunit,
+    loader,
+    tests
+  ], {
     annotation: 'dist'
   });
 };
+
+function loadWithInlineMap() {
+  return {
+    load: function (id) {
+      var code = fs.readFileSync(id, 'utf8');
+      var result = {
+        code: code,
+        map: null
+      };
+      var index = code.lastIndexOf(SOURCE_MAPPING_DATA_URL);
+      if (index === -1) {
+        return result;
+      }
+      result.code = code.slice(0, index);
+      result.map = parseSourceMap(code.slice(index + SOURCE_MAPPING_DATA_URL.length));
+      result.file = id;
+      return result;
+    }
+  };
+}
+
+function parseSourceMap(base64) {
+  return JSON.parse(new Buffer(base64, 'base64').toString('utf8'));
+}
---
+++ tests/foo.ts
@@ -0,0 +1,10 @@
+import {
+  Vector,
+} from 'scalar';
+
+QUnit.module('tests/foo');
+
+QUnit.test('init', function(assert) {
+  assert.expect(1);
+  assert.equal(true, true);
+});
